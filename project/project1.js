e8ddd6db1b8bef1d4a085fc9037c34f263461591
siddhantsaigaonkar <116439288+siddhantsaigaonkar@users.noreply.github.com>
2023-10-12 16:56:00 +0000

    configure code

--- project/project1.js
+++ project/project1.js
@@ -1,1 +1,14 @@
-console.log("hello sid");
+function calculateBMI(weight, height) {
+    // Write your code here
+    const bmi = weight / (height * height);
+
+    if (bmi < 18.5) {
+        return "Underweight";
+    } else if (bmi >= 18.5 || bmi < 24.9) {
+        return "Normal weight";
+    } else if (bmi >= 25 || bmi < 29.9) {
+        return "Overweight";
+    } else {
+        return "Obese";
+    }
+}
